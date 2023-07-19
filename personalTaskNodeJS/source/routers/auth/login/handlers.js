5a02ae825f043b89df898804debedd6c8d6b5330
Sennikov Danil <d.sennikov@brandmaker.ru>
2023-07-19 13:55:08 +0300

    промежуточные изменения по персональному проекту

--- personalTaskNodeJS/source/routers/auth/login/handlers.js
+++ personalTaskNodeJS/source/routers/auth/login/handlers.js
@@ -1,6 +1,15 @@
+import jwt from 'jsonwebtoken'
+import { getPassword } from '../../../helpers/env'
+
+const pass = getPassword();
+
 export const post = (req, res) => {
+
     try {
-        res.status(204).json()
+        const [ type, data ] = req.header('authorization').split(' ');
+        const [ email, password ] = Buffer.from(data, 'base64').toString().split(':');
+        req.session.user = { email }
+        res.status(204).json();
     } catch (error) {
         res.status(400).json({message: error.message})
     }
